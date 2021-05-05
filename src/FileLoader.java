9d0070d0ba5135cce22b0dcf47a97183aaae6291
WWykret <57493639+WWykret@users.noreply.github.com>
2021-05-05 16:30:31 +0200

    Added copying minecraft file to project directory

--- src/FileLoader.java
+++ src/FileLoader.java
@@ -1,34 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
-public class FileLoader{
-	private String path;
+public class FileLoader {
+    private String path;
 
-	public FileLoader(String _path) {
+    public FileLoader(String _path) {
+        this.path = _path;
+    }
 
-	}
+    public void ChangePath(String newPath) {
+        this.path = newPath;
+    }
 
-	public void ChangePath(String newPath) {
+    public String GetPath() {
+        return path;
+    }
 
-	}
+    public LoadedFiles LoadFiles() {
+        LoadedFiles result = new LoadedFiles();
 
-	public String GetPath() {
-		return null;
-	}
+        //DLA TESTOWANIA
+		path = "C:\\Users\\Admin\\AppData\\Roaming\\.minecraft\\versions\\1.16.5\\1.16.5.jar";
+		Path sourcePath = Paths.get(path);
+		//DLA TESTOWANIA
+		Path destPath;
+		try {
+			destPath = Paths.get(new File("./files/minecraft.jar").getCanonicalPath());
+			Files.copy(sourcePath, destPath);
+		} catch (IOException e) {
+			destPath = null;
+		}
 
-	public LoadedFiles LoadFiles() {
-		return null;
-	}
+        return result;
+    }
 }
 
 class LoadedFiles {
-	public Item[] items;
-	public Recepture[] receptures;
-	public boolean success;
+    public Item[] items;
+    public Recepture[] receptures;
+    public boolean success;
 
-	@Override
-	public boolean equals(Object obj) {
-	    if (obj == null || obj.getClass() != this.getClass()) return false;
-	    LoadedFiles files = (LoadedFiles) obj;
-		return Arrays.equals(files.items, this.items) && Arrays.equals(files.receptures, this.receptures) && files.success == this.success;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        LoadedFiles files = (LoadedFiles) obj;
+        return Arrays.equals(files.items, this.items) && Arrays.equals(files.receptures, this.receptures) && files.success == this.success;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
-public class Main{
-	public static Item[] GetItems() {
-		return null;
-	}
+public class Main {
+    public static void main(String[] args) {
+        FileLoader f = new FileLoader("");
+        f.LoadFiles();
+    }
 
-	public static void SetItems(Item[] _items) {
+    public static Item[] GetItems() {
+        return null;
+    }
 
-	}
+    public static void SetItems(Item[] _items) {
 
-	public static Recepture[] GetReceptures() {
-		return null;
-	}
+    }
 
-	public static void SetReceptures(Recepture[] _receptures) {
+    public static Recepture[] GetReceptures() {
+        return null;
+    }
 
-	}
+    public static void SetReceptures(Recepture[] _receptures) {
 
-	public static void Update() {
+    }
 
-	}
+    public static void Update() {
+
+    }
 }
